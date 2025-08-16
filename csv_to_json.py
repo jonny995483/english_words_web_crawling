9f16735550e8e1ece0572fb6541e626db28bf695
jonny995483 <jonny995483@gmail.com>
2025-08-17 04:35:02 +0900

    기존 csv 파일 난이도별로 json파일로 저장하기

---
+++ csv_to_json.py
@@ -0,0 +1,40 @@
+import csv
+import json
+
+FILENAME = '교육부_3천단어_수정분.csv'
+
+# cp949 인코딩으로 읽되, 오류가 발생하면 무시
+with open(FILENAME, 'r', newline='', encoding='utf-8', errors='ignore') as f:
+    lines = csv.reader(f)
+    header = next(lines)
+    print(header)
+
+    high = []
+    middle = []
+    low = []
+
+
+    for line in lines:
+        dict1 = {'id': line[0], header[1]: line[1], header[2]: line[2], header[3]: line[3]}
+        if line[4]:
+            dict1[header[4]] = line[4]
+        if line[5]:
+            dict1[header[5]] = line[5]
+
+        if line[3] == '초등':
+            low.append(dict1)
+
+        elif line[3] == '중고':
+            middle.append(dict1)
+
+        else:
+            high.append(dict1)
+
+with open('english_words_low.json', 'w', encoding='UTF-8') as f:
+    json.dump(low, f, ensure_ascii=False, indent=4)
+
+with open('english_words_middle.json', 'w', encoding='UTF-8') as f:
+    json.dump(middle, f, ensure_ascii=False, indent=4)
+
+with open('english_words_high.json', 'w', encoding='UTF-8') as f:
+    json.dump(high, f, ensure_ascii=False, indent=4)
